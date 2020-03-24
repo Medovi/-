e8de5efb051170303308c529e8452462ff7d5e2a
Medovi <62383546+Medovi@users.noreply.github.com>
2020-03-24 18:55:30 +0400

    Update library.h

--- library.h
+++ library.h
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-class Product {
-	string name, color;
-	int size1;
-public:
-	Product();
-	Product(string _name, string _color, int _size);
-	Product(const Product &p);
-	void print();
-	void SetName(string name_);
+class Product { // Создание нового класса Product
+	string name, color; // Создание полей с различными типами данных, которые по умолчанию ставятся с модификатором Private
+	int size1; 
+public: // Модификатор открытого доступа, так как по умолчанию ставится модификатор закрытого доступа Private (чуть выше)
+	Product(); // Создание конструктора "без параметров"
+	Product(string _name, string _color, int _size); // Создание конструктора "с параметрами"
+	Product(const Product &p); // Создание конструктора "копирования"
+	void print(); // Создание метода, для вывода данных и некоторых строк
+	void SetName(string name_); // Это и ниже - создание сетов и гетов, для ввода и вывода данных
 	string GetName();
 	void SetColor(string color_);
 	string GetColor();
